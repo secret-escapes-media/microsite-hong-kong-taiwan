3729781210ec38251ecd0e70d91b274a4ec6d2bb
hamishgray <hamish.gray@secretescapes.com>
2019-01-02 16:09:07 +0000

    Template updates

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('serve', ['build-jekyll'], function() {
     server: {
       baseDir: '_site/',
       routes: {
-        '/hong-kong-taiwan': '_site/'
+        '/metropolis-to-mountain': '_site/'
       }
     }
   });
@@ -118,8 +118,6 @@ gulp.task('build-main-js', function(cb) {
     './_assets/js/_components/standard.js',
     './_assets/js/_components/offer-countdown.js',
     './_assets/js/_components/modal.js',
-    './_assets/js/_components/modal-nav.js',
-    // './_assets/js/_components/sticky-nav.js',
 
     // custom js for project
     './_assets/js/main.js',
